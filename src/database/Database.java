c6d3d98e8d54b95341c12a252d588b6bbca7bf4b
lynnbswor <chiarac2708@gmail.com>
2024-12-27 18:07:00 +0100

    modifiche database

---
+++ src/database/Database.java
@@ -0,0 +1,56 @@
+package database;
+
+import entities.Abitazione;
+import entities.Prenotazione;
+import entities.Utente;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.HashMap;
+import java.util.List;
+
+public class Database {
+    private static HashMap<Integer, Utente> utenti = new HashMap<>();
+    private static HashMap<Integer, Abitazione> abitazioni = new HashMap<>();
+    private static HashMap<Integer, Prenotazione> prenotazioni = new HashMap<>();
+
+    public static void addUtente(Utente utente) {
+        utenti.put(utente.getId(), utente);
+    }
+    public static void removeUtente(Utente utente) {
+        utenti.remove(utente.getId());
+    }
+    public static void addAbitazione(Abitazione abitazione) {
+        abitazioni.put(abitazione.getId(), abitazione);
+    }
+    public static void removeAbitazione(Abitazione abitazione) {
+        abitazioni.remove(abitazione.getId());
+    }
+    public static void addPrenotazione(Prenotazione prenotazione) {
+        prenotazioni.put(prenotazione.getId(), prenotazione);
+    }
+    public static void removePrenotazione(Prenotazione prenotazione) {
+        prenotazioni.remove(prenotazione.getId());
+    }
+    public static List<Abitazione> getAbitazioniByCodiceHost (String codice) {
+        return abitazioni.values()
+                .stream()
+                .filter(abitazione -> codice.equals(abitazione.getCodiceHost()))
+                .toList();
+    }
+    public static Prenotazione getLastPrenotazioneByIdUtente (Integer id) {
+        Prenotazione p;
+        Long giorni = 9999L;
+        prenotazioni.values()
+                .stream()
+                .filter(prenotazione -> id.equals(prenotazione.getUtente().getId()))
+                .toList()
+                .forEach(prenotazione -> {
+                    if (ChronoUnit.DAYS.between(prenotazione.getStart(), LocalDate.now()) < giorni) {
+                        giorni = prenotazione.getStart();
+                        p = prenotazione;
+                    }
+                });
+                return p;
+    }
+}
